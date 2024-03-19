8c98b34f4effcbf5bec02cafeeb25cdc50490fe0
Jake-CSUMB <116601871+Jake-CSUMB@users.noreply.github.com>
2024-03-18 18:32:53 -0700

    Add links to ScheduleView and Transcript

--- src/components/student/StudentLayout.js
+++ src/components/student/StudentLayout.js
@@ -6,10 +6,10 @@ export const StudentHome = () => {
   return (
       <div>
           <h1>Student Home</h1>
-          <p>View class schedule. Drop course.</p>
+          <p><Link to="/schedule">View Class Schedule / Drop Course</Link></p>
           <p><Link to="/addCourse">Enroll in a Course</Link></p>
           <p><Link to="/studentAssignments">View Assignments and Grades</Link></p>
-          <p>View Transcript.</p>
+          <p><Link to="/transcript">View Transcript</Link></p>
       </div>
   );
 };
@@ -18,10 +18,10 @@ export const StudentLayout = () => {
   return (
     <>
       <nav>
-        <Link to="/">Home</Link> &nbsp;|&nbsp;   
+        <Link to="/">Home</Link> &nbsp;|&nbsp;
         <Link to="/schedule">View Class Schedule</Link>&nbsp;|&nbsp;
         <Link to="/addCourse">Enroll in a Course</Link>&nbsp;|&nbsp;
-        <Link to="/studentAssignments">View Assignments</Link>&nbsp;|&nbsp;  
+        <Link to="/studentAssignments">View Assignments</Link>&nbsp;|&nbsp;
         <Link to="/transcript">View Transcript</Link>
       </nav>
 
